8aa4dfbb2a1e2b2c2fab4f1fee5e33791f5e6778
engrron30 <ronaldtorresrosete30@gmail.com>
2025-10-07 14:17:21 +0800

    Remove irrelevant functions for mod_entry.c

--- 03-Module-with-Linking/mod_entry.c
+++ 03-Module-with-Linking/mod_entry.c
@@ -8,24 +8,17 @@ MODULE_AUTHOR("EngrRon");
 MODULE_DESCRIPTION("Kernel module with multiple functions");
 MODULE_VERSION("1.3");
 
-/* --- Parameters --- */
+/*********************************************************************
+ *
+ *	Insert Parameters for Module Here!
+ *
+ *********************************************************************/
 static int a = 5;
 static int b = 3;
 module_param(a, int, 0644);
 module_param(b, int, 0644);
 MODULE_PARM_DESC(a, "First integer");
 MODULE_PARM_DESC(b, "Second integer");
-
-/* --- Extra functions --- */
-static int add(int x, int y)
-{
-    return x + y;
-}
-
-static int mul(int x, int y)
-{
-    return x * y;
-}
 
 static int factorial(int n)
 {
@@ -40,9 +33,6 @@ static int factorial(int n)
 static int __init ronmod_init(void)
 {
     printk(KERN_INFO "[RONMOD] Module loaded with a=%d, b=%d\n", a, b);
-
-    printk(KERN_INFO "[RONMOD] add(%d, %d) = %d\n", a, b, add(a, b));
-    printk(KERN_INFO "[RONMOD] mul(%d, %d) = %d\n", a, b, mul(a, b));
     printk(KERN_INFO "[RONMOD] factorial(%d) = %d\n", a, factorial(a));
 
     return 0;
