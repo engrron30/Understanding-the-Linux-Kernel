b4c0a9a2c009fe5e39f3a37e686b5a4c5c9ba3e4
engrron30 <ronaldtorresrosete30@gmail.com>
2025-10-08 14:05:01 +0800

    Debug

---
+++ 99-Others/checksum/brcm_69xx/bcmdrivers/opensource/mod_entry.c
@@ -0,0 +1,34 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/moduleparam.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("EngrRon");
+MODULE_DESCRIPTION("Kernel module with multiple functions");
+MODULE_VERSION("1.3");
+
+/* --- Parameters --- */
+static int a = 5;
+static int b = 3;
+module_param(a, int, 0644);
+MODULE_PARM_DESC(a, "First integer");
+module_param(b, int, 0644);
+MODULE_PARM_DESC(b, "Second integer");
+
+/* --- Init / Exit --- */
+static int __init ronmod_init(void)
+{
+    printk(KERN_INFO "[RONMOD] Module loaded with a=%d, b=%d\n", a, b);
+
+    return 0;
+}
+
+static void __exit ronmod_exit(void)
+{
+    printk(KERN_INFO "[RONMOD] Module exiting...\n");
+}
+
+module_init(ronmod_init);
+module_exit(ronmod_exit);
+
