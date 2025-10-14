67572e2f0d39cc1627e630f1b682d2a78687e077
engrron30 <ronaldtorresrosete30@gmail.com>
2025-10-14 17:41:14 +0800

    Add file operation for ronmod module

---
+++ 04-Module-with-Userspace/ronmod.c
@@ -0,0 +1,36 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+/***********************************************************************************
+ *		Driver File Operations
+ ***********************************************************************************/
+struct file_operations ron_fops = {
+	.owner	= THIS_MODULE,
+	.open	= ron_open,
+}
+
+
+
+/***********************************************************************************
+ *		Module Entry and Exit
+ ***********************************************************************************/
+static int __init ronmod_init(void)
+{
+	printk(KERN_INFO "[RONMOD] Basic module is loaded!\n");
+	printk(KERN_DEBUG "[RONMOD] Basic Debug example.\n");
+	return 0;
+}
+
+static void __exit ronmod_exit(void)
+{
+	printk(KERN_WARNING "[RONMOD] Module is exiting...\n");
+}
+
+module_init(ronmod_init);
+module_exit(ronmod_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("EngrRon");
+MODULE_DESCRIPTION("Basic kernel module with printk example");
+MODULE_VERSION("1.0");
