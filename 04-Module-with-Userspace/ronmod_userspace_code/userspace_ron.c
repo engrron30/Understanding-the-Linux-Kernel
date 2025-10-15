d5fca30f84239b80eec7504d51860ac9fb4e6114
engrron30 <ronaldtorresrosete30@gmail.com>
2025-10-15 10:02:13 +0800

    Add access checker for ronmod module

--- 04-Module-with-Userspace/ronmod_userspace_code/userspace_ron.c
+++ 04-Module-with-Userspace/ronmod_userspace_code/userspace_ron.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
 
 #include "ronmod_def.h"
 
 int main()
 {
+	bool ret_val = false;
 	printf("Welcome to ronmod_ctl\n");
-	return 0;
+
+	if (access(RONMOD_DEV_PATH, F_OK) != 0)
+	{
+		printf("[USERSPACE] %s not found! Do mkod first.\n", RONMOD_DEV_PATH);
+		goto exit;
+	}
+
+	ret_val = true;
+exit:
+	return ret_val;
 }
