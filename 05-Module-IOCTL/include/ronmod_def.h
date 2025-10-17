6753a8270827626352201de9ef72fa64e0113796
engrron30 <ronaldtorresrosete30@gmail.com>
2025-10-17 17:45:00 +0800

    Define IOCTL Macros

--- 05-Module-IOCTL/include/ronmod_def.h
+++ 05-Module-IOCTL/include/ronmod_def.h
@@ -5,4 +5,12 @@
 #define RONMOD_DEV_PATH		DEV_PATH"/ronmod"
 //#define RONMOD_MAJOR_NUM	381
 
+// Magic number — must be unique per driver
+#define RONMOD_MAGIC 'r'
+
+// IOCTL commands
+#define RONMOD_CMD_HELLO		_IO(RONMOD_MAGIC, 0)
+#define RONMOD_CMD_SET_VALUE		_IOW(RONMOD_MAGIC, 1, int)
+#define RONMOD_CMD_GET_VALUE		_IOR(RONMOD_MAGIC, 2, int)
+
 #endif
