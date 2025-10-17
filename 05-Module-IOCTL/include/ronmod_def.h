290f40b0654aa51de64b27e647d795a403541161
engrron30 <ronaldtorresrosete30@gmail.com>
2025-10-17 17:55:00 +0800

    Update userspace code

--- 05-Module-IOCTL/include/ronmod_def.h
+++ 05-Module-IOCTL/include/ronmod_def.h
@@ -1,6 +1,8 @@
 #ifndef RONMOD_DEF_H
 #define RONMOD_DEF_H
 
+#include <linux/ioctl.h>
+
 #define DEV_PATH		"/dev"
 #define RONMOD_DEV_PATH		DEV_PATH"/ronmod"
 //#define RONMOD_MAJOR_NUM	381
--- 05-Module-IOCTL/ronmod_userspace_code/userspace_ron.c
+++ 05-Module-IOCTL/ronmod_userspace_code/userspace_ron.c
@@ -8,24 +8,34 @@
 int main()
 {
 	bool ret_val = false;
+	int val;
 	int fd = -1;
 	printf("Welcome to ronmod_ctl\n");
 
+	/* Access */
 #ifdef __CHECK_DEV_ACCESS__
 	if (access(RONMOD_DEV_PATH, F_OK) != 0)
 	{
 		printf("[USERSPACE] %s not found! Do mknod first.\n", RONMOD_DEV_PATH);
 		goto exit;
+	} else {
+		printf("[USERSPACE] %s found. Opening device...\n", RONMOD_DEV_PATH);
+		sleep(2);
 	}
 #endif
 
+	/* Open */
 	fd = open(RONMOD_DEV_PATH, O_RDWR);
 	if (fd < 0)
 	{
 		printf("[USERSPACE] Error to open device\n");
 		goto exit;
+	} else {
+		printf("[USERSPACE] %s Opened. Ioctl to device...\n", RONMOD_DEV_PATH);
+		sleep(2);
 	}
 
+	/* Ioctl */
 	printf("Sending HELLO command...\n");
 	ioctl(fd, RONMOD_CMD_HELLO);
 
@@ -37,6 +47,7 @@ int main()
 	ioctl(fd, RONMOD_CMD_GET_VALUE, &val);
 	printf("Kernel returned value = %d\n", val);
 
+	/* Close */
 	close(fd);
 	ret_val = true;
 	printf("[USERSPACE] Success\n");
